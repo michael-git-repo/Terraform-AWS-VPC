a6009193d8cb5681a19b4d116672eef134c11349
mike <bleosas222@gmail.com>
2026-09-09 00:04:56 +0100

    Increase EC2 root volume size

--- modules/web/main.tf
+++ modules/web/main.tf
@@ -123,7 +123,7 @@ resource "aws_launch_template" "this" {
     device_name = "/dev/xvda"
 
     ebs {
-      volume_size           = 8
+      volume_size           = 30
       volume_type           = "gp3"
       encrypted             = true
       delete_on_termination = true
